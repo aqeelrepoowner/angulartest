f4934a6f1e9065f4b66e69124194bc2f0a16e46d
Munshi <MunshiAk@ES.AD.ADP.com>
2018-02-03 12:33:03 +0530

    Added

---
+++ app/products/product-data.ts
@@ -0,0 +1,18 @@
+import { Product } from './product';
+
+export const PRODUCT_ITEMS: Product[] = [{
+  id: 1,
+  name: 'US Polo T-Shirt',
+  description: 'T Shirt',
+  price: 20
+}, {
+  id: 2,
+  name: 'Flying Machine Jeans',
+  description: 'Quality Jeans',
+  price: 30
+}, {
+  id: 3,
+  name: 'Fastrack Watch',
+  description: 'Watch the time',
+  price: 50 
+  }]
